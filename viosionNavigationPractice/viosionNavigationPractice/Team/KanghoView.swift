2876e589a2a7cb2834af2dc30991e2bd29c9f9ea
Kangho <kkh4323@naver.com>
2023-06-29 23:18:14 +0900

    Revert "add KanghoView, KanghoDetailView, KanghoPokemons"
    This reverts commit 04d9d7cae3d8606cb5b77c6fbb6662b283ada299.

--- viosionNavigationPractice/viosionNavigationPractice/Team/KanghoView.swift
+++ viosionNavigationPractice/viosionNavigationPractice/Team/KanghoView.swift
@@ -9,27 +9,7 @@ import SwiftUI
 
 struct KanghoView: View {
     var body: some View {
-        NavigationStack {
-            List(0..<pokemons.count, id: \.self) { index in
-                NavigationLink {
-                    KanghoDetailView(index: index, pokemon: "\(pokemons[index].name)")
-                } label: {
-                    AsyncImage(url: URL(string: "\(pokemons[index].image)")){ image in
-                        image
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: 60, height: 60)
-                            .clipShape(Circle())
-                    } placeholder: {
-                        ProgressView()
-                    }
-                    Text("\(pokemons[index].name)")
-                }
-
-                
-            }
-            .navigationTitle("🔥 💧 🌱 타입 포켓몬")
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
