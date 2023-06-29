22473b848a74d1026a163dd1f9b0d630fca518f2
Kangho <kkh4323@naver.com>
2023-06-29 19:29:54 +0900

    경환님께 턴을 넘깁니다

---
+++ viosionNavigationPractice/viosionNavigationPractice/viosionNavigationPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  viosionNavigationPracticeApp.swift
+//  viosionNavigationPractice
+//
+//  Created by KangHo Kim on 2023/06/29.
+//
+
+import SwiftUI
+
+@main
+struct viosionNavigationPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
